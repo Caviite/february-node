3a181d3a28abb43db69e419f3ea1b754a68b70c6
caviite <yomidave8682@gmail.com>
2026-03-31 11:56:10 +0100

    ddd

--- src/routes/cart.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addToCart, getCartItems, removeFromCart } = require('../contoller/cart');
-
-router.post('/add', addToCart);
-router.get('/', getCartItems);
-router.delete('/remove/:id', removeFromCart);
-
-module.exports = router;
